1d25ba7ad1082ed9aa4c3b800f980254fb6a1a67
Sujith Rajendran <s4152352@student.rmit.edu.au>
2026-03-02 10:53:11 +1100

    Added interactive menu

--- app.py
+++ app.py
@@ -1,17 +1,38 @@
-from flask import Flask
-from twilio.twiml.voice_response import VoiceResponse
+from flask import Flask, request
+from twilio.twiml.voice_response import VoiceResponse, Gather
 
 app = Flask(__name__)
 
 @app.route("/voice", methods=["POST"])
 def voice():
     response = VoiceResponse()
-    response.say("Hello Sujith, your Twilio voice application is working.")
+
+    # Create a Gather to collect keypad input
+    gather = Gather(num_digits=1, action="/gather", method="POST")
+    gather.say("Hello Sujith. Press 1 for a greeting. Press 2 to hear a fun fact.")
+    response.append(gather)
+
+    # If no input, repeat menu
+    response.redirect("/voice")
+
+    return str(response)
+
+# Handle the input
+@app.route("/gather", methods=["POST"])
+def gather():
+    response = VoiceResponse()
+    digit = request.values.get('Digits')
+
+    if digit == '1':
+        response.say("Hello! This is your greeting from the Twilio app.")
+    elif digit == '2':
+        response.say("Here is a fun fact: Honey never spoils!")
+    else:
+        response.say("Sorry, I did not understand.")
+        response.redirect("/voice")  # Go back to menu
+
     return str(response)
 
 @app.route("/")
 def home():
     return "Twilio Voice App Running"
-
-if __name__ == "__main__":
-    app.run(debug=True)
